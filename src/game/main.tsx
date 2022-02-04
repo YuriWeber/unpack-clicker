70851c009dda9d9cac8721f8c19cead8f762ecfa
Yuri Weber <a.yuriprof@gmail.com>
2022-02-04 10:48:20 -0300

    structure

---
+++ src/game/main.tsx
@@ -0,0 +1,9 @@
+export default function Unpack() {
+    return (
+        <header>
+            <img src="src/assets/others/logo.png" alt="logo.png" style={
+                {width: "300px"}
+            }/>
+        </header>
+    )
+}
