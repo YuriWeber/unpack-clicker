0fb12cd126b05595c0e4a175d97b9ec816de30cd
Yuri Weber <a.yuriprof@gmail.com>
2022-02-04 19:22:16 -0300

    header - main base

--- src/game/main.tsx
+++ src/game/main.tsx
@@ -1,9 +1,80 @@
 export default function Unpack() {
     return (
+        <>
         <header>
-            <img src="src/assets/others/logo.png" alt="logo.png" style={
-                {width: "300px"}
-            }/>
+            <img src="src/assets/placeholder/logo.png" alt="logo.png" className="header-logo"/>
+            <div className="menu">
+                <button className="save-button button">Save</button>
+                <button className="options-button button">Options</button>
+            </div>
         </header>
+
+        <div className="game">
+            <main>
+                <div className="boxes-area">
+                    <div className="box"><img src="src/assets/placeholder/box.png" alt="box.ng" /></div>
+                </div>
+
+                <div className="upgrades-container">
+                    <div className="move-bar">
+                        <div className="line"></div>
+                        <div className="line"></div>
+                        <div className="line"></div>
+                    </div>
+                    <div className="upgrades">
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                        <div className="upgrade-item-container">
+                            <button className="upgrade"><img src="src/assets/placeholder/upgrade.png" alt="upgrade.png" /></button>
+                            <span>CLICK</span>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            
+            <aside>
+                <h1 className="name">NAME</h1>
+                <h2 className="click-value">Click: 1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti obcaecati 
+                    eos quasi quam dolorum eaque assumenda, vitae corporis sed id in consequuntur
+                    aspernatur modi excepturi optio incidunt magnam. Dolorum, maiores.</p>  
+            </aside>
+        </div>
+
+        </>
     )
 }
